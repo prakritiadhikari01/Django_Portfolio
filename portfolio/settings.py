c3e7fae522e08f0c4baeebcd6d54840d25be04f0
prakritiadhikari01 <prakriti.adhikari.456@gmail.com>
2025-09-08 00:10:36 +0545

    Commit new changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='unsafe-local-key')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
