8f780cdaf180df78119bb94a4f98f1c7b5e62904
prakritiadhikari01 <prakriti.adhikari.456@gmail.com>
2025-09-16 23:37:30 +0545

    Projects

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('projects/', views.projects, name='projects'),
+    path("about/", views.about, name="about"),
+    path("certificates/", views.certificates, name="certificates"),
+    path("contact/", views.contact, name="contact"),
 ]
+
--- home/views.py
+++ home/views.py
@@ -8,5 +8,11 @@ def home(request):
 def about(request):
     return render(request, "home/about.html")
 
+def projects(request):
+    return render(request, 'home/projects.html')
+
 def certificates(request):
     return render(request, "home/certificates.html")
+
+def contact(request):
+    return render(request, "home/contact.html")
